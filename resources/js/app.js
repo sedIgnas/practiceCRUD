f9b1564294365c293aee9586e684cd9484874f3c
Ignas Šed <ignas.seduikis@gmail.com>
2022-03-22 17:40:37 +0200

    created a component for upcoming content and initiated vue compiler

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
 require('./bootstrap');
 
-import Alpine from 'alpinejs';
+import { createApp } from 'vue'
+import PostsIndex from './components/Posts/Index'
 
-window.Alpine = Alpine;
-
-Alpine.start();
+const app = createApp({})
+app.component('posts-index', PostsIndex)
+app.mount('#app')
