564d7713f45c575b65867795c60635a594fd9500
Ignas Šed <ignas.seduikis@gmail.com>
2022-03-30 17:12:24 +0300

    reworked post loading. offloaded some logic to composition

---
+++ resources/js/composables/posts.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import { ref } from 'vue'
+
+export default function usePosts() {
+  const posts = ref([])
+  const getPosts = async () => {
+    axios.get('/api/posts')
+    .then(response => {
+      posts.value = response.data;
+    })
+  }
+
+  return { posts, getPosts}
+}
