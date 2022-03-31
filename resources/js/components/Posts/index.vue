6117e053ea0bdc4a65c9c2e8b4b756c261e26991
Ignas Šed <ignas.seduikis@gmail.com>
2022-03-31 13:32:57 +0300

    added categories composable for sorting dropdown, dropdown now lists catogories from new categories api

--- resources/js/components/Posts/index.vue
+++ resources/js/components/Posts/index.vue
@@ -1,67 +1,186 @@
 <template>
   <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
-        <div class="min-w-full align-middle">
-            <table class="min-w-full divide-y divide-gray-200 border">
-                <thead>
-                <tr>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">ID</span>
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</span>
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Category</span>
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span>
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Created at</span>
-                    </th>
-                </tr>
-                </thead>
-                <tbody class="bg-white divide-y divide-gray-200 divide-solid">
-                <tr v-for="post in posts.data" :key="post.id">
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.id }}</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.title }}</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.category }}</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.content }}</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.created_at }}</td>
-                </tr>
-                </tbody>
-            </table>
+    <div class="min-w-full align-middle">
+      <div class="mb-4">
+                <select v-model="selectedCategory" class="block mt-1 w-full sm:w-1/4 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                    <option value="" selected>-- Filter by category --</option>
+                    <option v-for="category in categories" :value="category.id">
+                        {{ category.name }}
+                    </option>
+                </select>
+            </div>
+      <table class="min-w-full divide-y divide-gray-200 border">
+        <thead>
+          <tr>
+            <th class="px-6 py-3 bg-gray-50 text-left">
+              <span
+                class="
+                  text-xs
+                  leading-4
+                  font-medium
+                  text-gray-500
+                  uppercase
+                  tracking-wider
+                "
+                >ID</span
+              >
+            </th>
+            <th class="px-6 py-3 bg-gray-50 text-left">
+              <span
+                class="
+                  text-xs
+                  leading-4
+                  font-medium
+                  text-gray-500
+                  uppercase
+                  tracking-wider
+                "
+                >Title</span
+              >
+            </th>
+            <th class="px-6 py-3 bg-gray-50 text-left">
+              <span
+                class="
+                  text-xs
+                  leading-4
+                  font-medium
+                  text-gray-500
+                  uppercase
+                  tracking-wider
+                "
+                >Category</span
+              >
+            </th>
+            <th class="px-6 py-3 bg-gray-50 text-left">
+              <span
+                class="
+                  text-xs
+                  leading-4
+                  font-medium
+                  text-gray-500
+                  uppercase
+                  tracking-wider
+                "
+                >Content</span
+              >
+            </th>
+            <th class="px-6 py-3 bg-gray-50 text-left">
+              <span
+                class="
+                  text-xs
+                  leading-4
+                  font-medium
+                  text-gray-500
+                  uppercase
+                  tracking-wider
+                "
+                >Created at</span
+              >
+            </th>
+          </tr>
+        </thead>
+        <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+          <tr v-for="post in posts.data" :key="post.id">
+            <td
+              class="
+                px-6
+                py-4
+                whitespace-no-wrap
+                text-sm
+                leading-5
+                text-gray-900
+              "
+            >
+              {{ post.id }}
+            </td>
+            <td
+              class="
+                px-6
+                py-4
+                whitespace-no-wrap
+                text-sm
+                leading-5
+                text-gray-900
+              "
+            >
+              {{ post.title }}
+            </td>
+            <td
+              class="
+                px-6
+                py-4
+                whitespace-no-wrap
+                text-sm
+                leading-5
+                text-gray-900
+              "
+            >
+              {{ post.category }}
+            </td>
+            <td
+              class="
+                px-6
+                py-4
+                whitespace-no-wrap
+                text-sm
+                leading-5
+                text-gray-900
+              "
+            >
+              {{ post.content }}
+            </td>
+            <td
+              class="
+                px-6
+                py-4
+                whitespace-no-wrap
+                text-sm
+                leading-5
+                text-gray-900
+              "
+            >
+              {{ post.created_at }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
-            <Pagination :data="posts" @pagination-change-page="getPosts" />
-        </div>
+      <Pagination :data="posts" @pagination-change-page="getPosts" />
     </div>
+  </div>
 </template>
 
 <script>
-import { onMounted } from 'vue'
-import usePosts from '../../composables/posts'
+import { ref, onMounted } from "vue";
+import usePosts from "../../composables/posts";
+import useCategories from "../../composables/categories";
 
-  export default {
-    setup() {
-      const { posts, getPosts} = usePosts()
-      onMounted(getPosts)
-      
-      return { posts, getPosts }
-    }
-    // data() {
-    //   return {
-    //     posts: []
-    //   }
-    // },
-    // mounted() {
-    //   this.fetchPosts();
-    // },
-    // methods: {
-    //   fetchPosts() {
-    //     axios.get('/api/posts')
-    //     .then(response => this.posts = response.data)
-    //     .catch(error => console.log(error))
-    //   }
-    // }
-  }
+export default {
+  setup() {
+    const selectedCategory = ref('');
+    const { posts, getPosts } = usePosts();
+    const { categories, getCategories } = useCategories();
+    onMounted(() => {
+      getPosts()
+      getCategories()
+    });
+
+    return { posts, getPosts, categories };
+  },
+  // data() {
+  //   return {
+  //     posts: []
+  //   }
+  // },
+  // mounted() {
+  //   this.fetchPosts();
+  // },
+  // methods: {
+  //   fetchPosts() {
+  //     axios.get('/api/posts')
+  //     .then(response => this.posts = response.data)
+  //     .catch(error => console.log(error))
+  //   }
+  // }
+};
 </script>
---
+++ resources/js/composables/categories.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import { ref } from 'vue'
+
+export default function useCategories() {
+  const categories = ref({});
+  const getCategories = async () => {
+    axios.get('/api/categories')
+      .then(response => {
+        categories.value = response.data.data;
+      })
+  }
+
+  return { categories, getCategories }
+}
