5b428702095a884ea9d920db2fc0bd290065e623
Ignas Šed <ignas.seduikis@gmail.com>
2022-04-01 15:19:44 +0300

    title change according to route, parameters, meta.

--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -3,8 +3,8 @@ import PostsIndex from '../components/Posts/Index'
 import PostsCreate from '../components/Posts/Create'
 
 const routes = [
-  { path: '/', name: 'posts.index', component: PostsIndex },
-  { path: '/posts/create', name: 'posts.create', component: PostsCreate }
+  { path: '/', name: 'posts.index', component: PostsIndex, meta: { title: 'Posts' } },
+  { path: '/posts/create', name: 'posts.create', component: PostsCreate, meta: { title: 'Add new post' } }
 ];
 
 export default createRouter({
