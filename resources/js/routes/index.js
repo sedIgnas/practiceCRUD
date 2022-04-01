d10bf39d9ada37fc44b4590e96eeda339344f4c9
Ignas Šed <ignas.seduikis@gmail.com>
2022-04-01 15:11:32 +0300

    routes cleanup, separate file for routes

---
+++ resources/js/routes/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import PostsIndex from '../components/Posts/Index'
+import PostsCreate from '../components/Posts/Create'
+
+const routes = [
+  { path: '/', name: 'posts.index', component: PostsIndex },
+  { path: '/posts/create', name: 'posts.create', component: PostsCreate }
+];
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
