8282cab9852d234fa15ff8d83089a3ac5340bbe4
Ignas Šed <ignas.seduikis@gmail.com>
2022-04-06 15:43:36 +0300

    posts filtering. by single, multiple or global filters

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,52 +10,68 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
-    {
-        $orderColumn = request('order_column', 'created_at');
-        if (!in_array($orderColumn, ['id', 'title', 'created_at'])) {
-            $orderColumn = 'created_at';
-        }
-        $orderDirection = request('order_direction', 'desc');
-        if (!in_array($orderDirection, ['asc', 'desc'])) {
-            $orderDirection = 'desc';
-        }
-        $posts = Post::with('category')
-            ->when(request('category'), function ($query) {
-                $query->where('category_id', request('category'));
-            })
-            ->orderBy($orderColumn, $orderDirection)
-            ->paginate(50);
-        return PostResource::collection($posts);
+  public function index()
+  {
+    $orderColumn = request('order_column', 'created_at');
+    if (!in_array($orderColumn, ['id', 'title', 'created_at'])) {
+      $orderColumn = 'created_at';
     }
-
-    public function store(StorePostRequest $request)
-    {
-        if ($request->hasFile('thumbnail')) {
-            $filename = $request->file('thumbnail')->getClientOriginalName();
-            info($filename);
-        }
-        $post = Post::create($request->validated());
-
-        return new PostResource($post);
+    $orderDirection = request('order_direction', 'desc');
+    if (!in_array($orderDirection, ['asc', 'desc'])) {
+      $orderDirection = 'desc';
     }
+    $posts = Post::with('category')
+      ->when(request('search_category'), function ($query) {
+        $query->where('category_id', request('search_category'));
+      })
+      ->when(request('search_id'), function ($query) {
+        $query->where('id', request('search_id'));
+      })
+      ->when(request('search_title'), function ($query) {
+        $query->where('title','like', '%'.request('search_title').'%');
+      })
+      ->when(request('search_content'), function ($query) {
+        $query->where('content','like', '%'.request('search_content').'%');
+      })
+      ->when(request('search_global'), function ($query) {
+        $query->where(function($q) {
+        $q->where('id', request('search_global'))
+          ->orWhere('title','like', '%'.request('search_global').'%')
+          ->orWhere('content','like', '%'.request('search_global').'%');
+        });
+      })
+      ->orderBy($orderColumn, $orderDirection)
+      ->paginate(50);
+    return PostResource::collection($posts);
+  }
 
-    public function show(Post $post)
-    {
-        return new PostResource($post);
+  public function store(StorePostRequest $request)
+  {
+    if ($request->hasFile('thumbnail')) {
+      $filename = $request->file('thumbnail')->getClientOriginalName();
+      info($filename);
     }
+    $post = Post::create($request->validated());
 
-    public function update(Post $post, StorePostRequest $request)
-    {
-        $post->update($request->validated());
+    return new PostResource($post);
+  }
 
-        return new PostResource($post);
-    }
+  public function show(Post $post)
+  {
+    return new PostResource($post);
+  }
 
-    public function destroy(Post $post)
-    {
-      $post->delete();
+  public function update(Post $post, StorePostRequest $request)
+  {
+    $post->update($request->validated());
 
-      return response()->noContent();
-    }
+    return new PostResource($post);
+  }
+
+  public function destroy(Post $post)
+  {
+    $post->delete();
+
+    return response()->noContent();
+  }
 }
