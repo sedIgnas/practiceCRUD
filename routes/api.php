c9b6f73371efb3d6db49d0433a3fc05c29347565
Ignas Šed <ignas.seduikis@gmail.com>
2022-04-11 16:17:08 +0300

    Login cleanup, laravel sanctum authentication, redirect to login screen if not logged in

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::group(['middleware' => 'auth:sanctum'], function () {
+  Route::apiResource('posts', PostController::class);
+  Route::get('categories', [CategoryController::class, 'index']);
+  Route::get('/user', function (Request $request) {
     return $request->user();
+  });
 });
-
-Route::apiResource('posts', PostController::class);
-Route::get('categories', [CategoryController::class, 'index']);
